692e1c8ff4b7e81cecf1fa001774e82669bc2fc8
spiner321 <luckyboy3214@naver.com>
2022-05-24 11:48:31 +0900

    project 생성

---
+++ cakd_dj/blog/urls.py
@@ -0,0 +1,8 @@
+from urllib.parse import urlparse
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('<int:pk>', views.PostDetail.as_view()),
+    path('', views.PostList.as_view()),
+]
---
+++ cakd_dj/blog/views.py
@@ -0,0 +1,12 @@
+# Create your views here.
+
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Post
+
+class PostList(ListView):
+    model = Post
+    ordering = '-pk'
+
+class PostDetail(DetailView):
+    model = Post
