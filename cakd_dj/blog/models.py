8de0de7b8bec933e7bf7835bb99f86970cd1468e
spiner321 <luckyboy3214@naver.com>
2022-05-26 12:44:55 +0900

    작성자 카테고리 구현

--- cakd_dj/blog/models.py
+++ cakd_dj/blog/models.py
@@ -1,9 +1,20 @@
-# Create your models here.
-
 import os
 from django.db import models
+from django.contrib.auth.models import User
 
+class Category(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
 
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return f'/blog/category/{self.slug}/'
+
+    class Meta:
+        verbose_name_plural = 'categories'
+        
 class Post(models.Model):
     title = models.CharField(max_length=30)
     hook_text = models.CharField(max_length=100, blank=True)
@@ -14,6 +25,10 @@ class Post(models.Model):
     
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    
+    author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+    
+    category = models.ForeignKey(Category, null=True, blank=True, on_delete=models.SET_NULL)
    
     # author: 추후 작성 예정
     def __str__(self):
--- cakd_dj/blog/urls.py
+++ cakd_dj/blog/urls.py
@@ -1,8 +1,8 @@
-from urllib.parse import urlparse
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<int:pk>', views.PostDetail.as_view()),
-    path('', views.PostList.as_view()),
+    path('category/<str:slug>/', views.category_page),     
+    path('<int:pk>/', views.PostDetail.as_view()),
+    path('', views.PostList.as_view()),    
 ]
