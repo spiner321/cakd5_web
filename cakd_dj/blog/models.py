55097dfa56e21f41ff2f716d21559555674aaedd
spiner321 <luckyboy3214@naver.com>
2022-06-02 11:25:37 +0900

    댓글 생성, 수정 및 삭제 기능 구현

--- cakd_dj/blog/models.py
+++ cakd_dj/blog/models.py
@@ -1,7 +1,6 @@
 import os
 from django.db import models
 from django.contrib.auth.models import User
-from django.contrib.auth.models import User
 from markdownx.models import MarkdownxField
 from markdownx.utils import markdown
 
@@ -14,7 +13,6 @@ class Tag(models.Model):
 
     def get_absolute_url(self):
         return f'/blog/tag/{self.slug}/'
-
 class Category(models.Model):
     name = models.CharField(max_length=50, unique=True)
     slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
@@ -38,12 +36,12 @@ class Post(models.Model):
     
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-
+    
     author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
     
     category = models.ForeignKey(Category, null=True, blank=True, on_delete=models.SET_NULL)
-
-    tags = models.ManyToManyField(Tag,blank=True)
+    
+    tags = models.ManyToManyField(Tag, blank=True)
    
     # author: 추후 작성 예정
     def __str__(self):
@@ -60,3 +58,16 @@ class Post(models.Model):
 
     def get_content_markdown(self):
         return markdown(self.content)
+    
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.author}::{self.content}'
+
+    def get_absolute_url(self):
+        return f'{self.post.get_absolute_url()}#comment-{self.pk}'
